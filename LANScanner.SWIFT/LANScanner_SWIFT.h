c19a9651bbd1eb9fef2b3b232da2ad14bdf0e85b
Daian <daian.aiziatov@gmail.com>
2019-02-27 13:20:22 -0500

    Initial commit

---
+++ LANScanner.SWIFT/LANScanner_SWIFT.h
@@ -0,0 +1,19 @@
+//
+//  LANScanner_SWIFT.h
+//  LANScanner.SWIFT
+//
+//  Created by Daian Aiziatov on 27/02/2019.
+//  Copyright © 2019 Daian Aiziatov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LANScanner_SWIFT.
+FOUNDATION_EXPORT double LANScanner_SWIFTVersionNumber;
+
+//! Project version string for LANScanner_SWIFT.
+FOUNDATION_EXPORT const unsigned char LANScanner_SWIFTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LANScanner_SWIFT/PublicHeader.h>
+
+
